1ae37578b5e9309c74e4e85333ed18b396a224ed
RodneyDickson <43690520+RodneyDickson@users.noreply.github.com>
2021-10-07 14:18:38 -0400

    initial commit
    behaviours scripts dont work, most scripts dont work, not too sure why
    player behaviour worked to move across the map, but now all the scripts are saying the names are wrong

---
+++ Assets/[Scripts]/BulletManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BulletManager : MonoBehaviour
+{
+    public Queue<GameObject> bulletPool;
+    public int bulletNumber;
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletPool = new Queue<GameObject>(); // creates an empty queue
+
+        BuildBulletPool();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void BuildBulletPool()
+    {
+        for(int i = 0; i < bulletNumber; i++)
+        {
+            var temp_bullet = Instantiate(bulletPrefab);
+            temp_bullet.SetActive(false);
+            temp_bullet.transform.parent = transform;
+            bulletPool.Enqueue(temp_bullet);
+        }
+    }
+
+    public GameObject GetBullet(Vector2 position)
+    {
+        var temp_bullet = bulletPool.Dequeue();
+        temp_bullet.transform.position = position;
+        temp_bullet.SetActive(true);
+
+        return temp_bullet;
+    }
+
+    public void ReturnBullet(GameObject returnedBullet)
+    {
+        returned_bullet.SetActive(false);
+        bulletPool.Enqueue(returnedBullet);
+    }
+}
